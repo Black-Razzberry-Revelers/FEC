842406c3a4332c8a6b0bfb5a870287f4c0222658
Tammam Tautou <tautoutammam@gmail.com>
2023-05-05 14:06:41 -0700

    finish reviews routes

--- server/routes.js
+++ server/routes.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const controllers = require('./controllers')
+const controllers = require('./controllers');
 
 const router = express.Router();
-//Overview
+// Overview
 
-//Products
+// Products
 
-//Reviews
+// Reviews
 router.get('/reviews', controllers.reviews.getListReviews);
+router.get('/reviews/meta', controllers.reviews.getReviewsMetadata);
+router.post('/reviews', controllers.reviews.postReview);
+router.put('/reviews/:review_id/helpful', controllers.reviews.putReviewAsHelpful);
+router.put('/reviews/:review_id/report', controllers.reviews.putReportReview);
 
-//Questions and Answers
+// Questions and Answers
 
-//Cart
+// Cart
 
-//Ineractions
+// Ineractions
 
 module.exports = router;
-
