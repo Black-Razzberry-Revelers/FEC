9c1077547497c706fe5c56de7ab626b464fe9c0b
Benji Cardon <Benjaminlibrarian@gmail.com>
2023-05-07 08:40:48 -0600

    I've basically just added the skeleton of my Backend. Copied Heavily from Tammam's Routes

--- server/routes.js
+++ server/routes.js
@@ -14,6 +14,13 @@ router.put('/reviews/:review_id/helpful', controllers.reviews.putReviewAsHelpful
 router.put('/reviews/:review_id/report', controllers.reviews.putReportReview);
 
 // Questions and Answers
+router.get('/questions', controllers.questions.getQuestions);
+router.get('/questions/answers', controllers.questions.getAnswers);
+router.post('/questions/question', controllers.questions.postQuestion);
+router.post('/questions/answer', controllers.questions.postAnswer);
+router.put('/questions/question/:question_id/helpful', controllers.questions.putQuestionAsHelpful);
+router.put('/questions/answer/:answer_id/helpful', controllers.questions.putAnswerAsHelpful);
+router.put('/question/answer/:answer_id/report', controllers.questions.putAnswerAsReported);
 
 // Cart
 
