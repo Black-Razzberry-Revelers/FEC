f8948b169cfd4e86926e89361b6b599b1c53cf35
Dillon Armstrong <dstrongarm93@gmail.com>
2023-05-08 12:05:01 -0600

    edits to mock data

--- client/src/components/Overview/index.jsx
+++ client/src/components/Overview/index.jsx
@@ -3,15 +3,21 @@ import ProductInfo from './productInfo';
 import StyleSelect from './styleSelect';
 import Gallery from './Gallery';
 import AddToCart from './addToCart';
+import { productsData, productData, stylesData } from './mockData';
 
 export default function Overview() {
-  const [style, setStyle] = React.useState({});
+  const [styles, setStyles] = React.useState(stylesData.results);
+  const [style, setStyle] = React.useState(styles[0]);
+
+  const styleContext = React.createContext(style);
 
   return (
     <div>
-      <ProductInfo style={style} />
       <StyleSelect style={style} setStyle={setStyle} />
-      <Gallery style={style} />
+      <styleContext.Provider value={style.name}>
+        <ProductInfo />
+        <Gallery />
+      </styleContext.Provider>
       <AddToCart />
     </div>
   );
