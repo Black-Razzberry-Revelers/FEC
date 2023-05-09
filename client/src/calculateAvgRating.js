718e675a0547c195de305a166e0a5f8700b388bb
Dillon Armstrong <dstrongarm93@gmail.com>
2023-05-09 12:47:19 -0600

    merged with latest main. no conflicts

---
+++ client/src/calculateAvgRating.js
@@ -0,0 +1,19 @@
+export default function avgRating(ratingsObj) {
+  let responses = 0;
+  let score = 0;
+  const ratings = Object.values(ratingsObj);
+
+  ratings.forEach((rating, i) => {
+    responses += (Number(rating)); // does it really come through as a string??
+    score += (rating * (i + 1));
+  });
+
+  const average = score / responses;
+
+  return Math.round(average * 10) / 10; // round to nearest 10th.
+};
+
+// let test = { '1': '10', '2': '15', '3': '20', '4': '25', '5': '50' }
+
+// console.log(avgRating(test));
+// client/src/calculateAvgRating.js
--- client/src/components/Overview/productInfo/index.jsx
+++ client/src/components/Overview/productInfo/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Price from './price';
 import Share from './share';
+import Description from './description';
+import { styleContext } from '../index';
 
-export default function ProductInfo() {
+export default function ProductInfo({ product, avgRating }) {
+  const { style } = React.useContext(styleContext);
+
   return (
     <>
-      <h1>ProductInfo</h1>
-      <div>name</div>
-      <div>category</div>
-      <div>rating</div>
+      <h1>
+        {product.name}
+        <div> {avgRating} </div>
+      </h1>
+      <div>in: {product.category}</div>
+      <Description product={product}/>
       <Price />
       <Share />
     </>
