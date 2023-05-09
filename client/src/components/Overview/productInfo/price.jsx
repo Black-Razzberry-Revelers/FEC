94b4ee7f8155a81cd8e449882a9b792713075de8
Dillon Armstrong <dstrongarm93@gmail.com>
2023-05-09 14:52:17 -0600

    merging with product info

--- client/src/components/Overview/productInfo/price.jsx
+++ client/src/components/Overview/productInfo/price.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+import { styleContext } from '../index';
 
 export default function Price() {
-  const [onSale, setOnSale] = React.useState(false);
+  const { style } = React.useContext(styleContext);
   // if onSale cross out default price and display sale price
   return (
-    <h1>ProductInfo</h1>
+    style.sale_price
+      ? <div> {style.sale_price} </div>
+      : <div> {style.original_price} </div>
   );
 }
