0c59adb4ab35d4fc0629d13acd833771aabbe324
Tammam Tautou <tautoutammam@gmail.com>
2023-05-08 19:29:34 -0700

    add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,17 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
     camelcase: 'off',
-    "no-console": "off"
-
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
   },
 };
