9b8d3bbcee0c7b78e77f92b54ed9f519418a57f1
Tammam Tautou <tautoutammam@gmail.com>
2023-05-10 19:13:03 -0700

    add a fetcher to fetch data from server

---
+++ client/src/components/fetcher.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import axios from 'axios';
+
+export default {
+  getListReviews: (
+    page = 1,
+    count = 5,
+    sort = 'newest',
+    product_id = 40344,
+  ) => {
+    const options = {
+      url: 'api/reviews',
+      method: 'get',
+      params: {
+        page,
+        count,
+        sort,
+        product_id,
+      },
+    };
+    return axios(options);
+  },
+
+  getReviewsMetadata: (product_id = 40344) => {
+    const options = {
+      url: 'api/reviews/meta',
+      method: 'get',
+      params: {
+        product_id,
+      },
+    };
+    return axios(options);
+  },
+
+  addReview: (
+    product_id,
+    rating,
+    summary,
+    body,
+    recommend,
+    name,
+    email,
+    photos,
+    characteristics,
+  ) => {
+    const options = {
+      url: 'api/reviews',
+      method: 'post',
+      data: {
+        product_id,
+        rating,
+        summary,
+        body,
+        recommend,
+        name,
+        email,
+        photos,
+        characteristics,
+      },
+    };
+    return axios(options);
+  },
+
+  markReviewAsHelpful: (review_id) => {
+    const options = {
+      url: `api/reviews/${review_id}/helpful`,
+      method: 'put',
+    };
+    return axios(options);
+  },
+
+  reportReview: (review_id) => {
+    const options = {
+      url: `api/reviews/${review_id}/report`,
+      method: 'put',
+    };
+    return axios(options);
+  },
+};
