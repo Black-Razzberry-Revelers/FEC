6bf4c62a8d6940b540af46cc0580678774b63954
Andrew Lund <andrew.s.lund@gmail.com>
2023-05-12 15:53:15 -0700

    added PropTypes for ItemCard component

--- client/src/components/Related/subcomponents/ItemCard.jsx
+++ client/src/components/Related/subcomponents/ItemCard.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import ComparisonModal from './ComparisonModal';
 import { styleContext } from '../../App';
 
@@ -8,7 +9,7 @@ export default function ItemCard({
   const {
     style, setStyle, styles, setStyles,
   } = useContext(styleContext);
-  const defaultStyle = item.styles.filter((style) => style['default?'] === true);
+  const defaultStyle = item.styles.filter((itemStyle) => itemStyle['default?'] === true);
   const placeholder = 'https://static-00.iconduck.com/assets.00/image-icon-256x256-09od4zyo.png';
   const thumbnail = defaultStyle.length > 0 ? defaultStyle[0].photos[0].thumbnail_url
     : item.styles[0].photos[0].thumbnail_url;
@@ -25,7 +26,6 @@ export default function ItemCard({
           setStyle(defaultStyle[0]);
         }}
         onKeyDown={(e) => {
-          console.log('Keyboard Event:', e);
           if (e.code === 'Enter') {
             setProduct(item.product);
             setStyles(item.styles);
@@ -78,3 +78,46 @@ export default function ItemCard({
     </>
   );
 }
+
+ItemCard.propTypes = {
+  item: PropTypes.shape({
+    product: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      campus: PropTypes.string,
+      name: PropTypes.string,
+      slogan: PropTypes.string,
+      description: PropTypes.string,
+      category: PropTypes.string,
+      default_price: PropTypes.string,
+      created_at: PropTypes.string,
+      updated_at: PropTypes.string,
+      features: PropTypes.arrayOf(
+        PropTypes.shape({ feature: PropTypes.string, value: PropTypes.string }),
+      ),
+    }),
+    styles: PropTypes.arrayOf(PropTypes.shape({
+      style_id: PropTypes.number,
+      name: PropTypes.string,
+      original_price: PropTypes.string,
+      sale_price: PropTypes.string,
+      'default?': PropTypes.bool,
+      photos: PropTypes.arrayOf(PropType.shape({
+        thumbnail_url: PropTypes.string,
+        url: PropTypes.string,
+      })),
+      skus: PropTypes.objectOf(PropTypes.shape({
+        quantity: PropTypes.number,
+        size: PropTypes.string,
+      })),
+    })),
+  }).isRequired,
+  outfitItems: PropTypes.arrayOf(ItemCard.propTypes.item),
+  setOutfitItems: PropTypes.func.isRequired,
+  outfitList: PropTypes.bool,
+  setProduct: PropTypes.func.isRequired,
+};
+
+ItemCard.defaultProps = {
+  outfitList: false,
+  outfitItems: [],
+};
