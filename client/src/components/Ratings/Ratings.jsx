eca6de489453c56ee2cb9a14f2a0981a201bfd34
Tammam Tautou <tautoutammam@gmail.com>
2023-05-12 00:16:39 -0700

    modify

--- client/src/components/Ratings/Ratings.jsx
+++ client/src/components/Ratings/Ratings.jsx
@@ -1,38 +1,41 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import fetcher from '../fetcher';
 import ReviewsList from './sub-comps/ReviewsList';
 
 function Ratings(/* {product_id, count} */) {
   const [reviews, setReviews] = useState();
-  const [more, setMore] = useState(false);
-  const [sort, setSort] = useState('relevant');
+  const [moreReviews, setMoreReviews] = useState(false);
+  const [sortReviews, setSortReviews] = useState('relevant');
+
   useEffect(() => {
     fetcher
-      .getListReviews(1, 1000, sort, 40346)
+      .getListReviews(1, 1000, sortReviews, 40346)
 
       .then((result) => {
-        console.log('????', result.data.results.length);
+        // console.log('????', result.data.results.length);
         setReviews(result.data.results);
       })
       .catch((err) => console.log('RATINGS GET LIST REVIEWS ERR', err));
-  }, [sort]);
+  }, [sortReviews]);
   return (
     <>
       <h2>Ratings & Reviews</h2>
       <h3>{reviews && `${reviews.length} Reviews, sorted by`}</h3>
-      <select onChange={(e) => setSort(e.target.value)}>
+      <select onChange={(e) => setSortReviews(e.target.value)}>
         <option value="relevant">Relevant</option>
         <option value="helpful">Helpful</option>
         <option value="newest">Newest</option>
       </select>
       <div>
         {reviews && (
-          <ReviewsList reviews={more ? reviews : reviews.slice(0, 2)} />
+          <ReviewsList
+            reviews={moreReviews ? reviews : reviews.slice(0, 2)}
+
+          />
         )}
       </div>
-      <button type="button" onClick={() => setMore(true)}>
+      <button type="button" onClick={() => setMoreReviews(true)}>
         More Reviews
       </button>
     </>
