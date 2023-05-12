40f724fa20d55b75237e2ffbefb820f8d308845c
Dillon Armstrong <dstrongarm93@gmail.com>
2023-05-12 17:19:13 -0600

    merged with main

--- client/src/components/Overview/index.jsx
+++ client/src/components/Overview/index.jsx
@@ -4,7 +4,7 @@ import StyleSelect from './styleSelect';
 import Gallery from './Gallery';
 import AddToCart from './addToCart';
 import { styleContext } from '../App';
-import Stars from '../stars';
+import Stars from
 
 export default function Overview({ avgRating, product }) {
   const { style, setStyle, styles } = React.useContext(styleContext);
@@ -32,27 +32,26 @@ export default function Overview({ avgRating, product }) {
   }, [style]);
 
   return (
-    <div data-testid="overview">
+    <>
       <h1>
         {product.name}
       </h1>
       <div>
-        in: {product.category}
-      </div>
-      <div className="rating">
         {avgRating}
-        <Stars />
+      </div>
+      <div>
+        in: {product.category}
       </div>
       <div className="overview">
         <div className="stylesinfo">
           <ProductInfo product={product} avgRating={avgRating} />
           <StyleSelect styles={styles} />
-          <AddToCart sizes={sizes} />
+          <AddToCart sizes={sizes} product={product} />
         </div>
         <div>
           <Gallery gallery={gallery} display={display} setDisplay={setDisplay} />
         </div>
       </div>
-    </div>
+    </>
   );
 }
