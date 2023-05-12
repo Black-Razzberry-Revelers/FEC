bf4d4525eff90b7058faa4d9ba257a1c4ff0a893
Tammam Tautou <tautoutammam@gmail.com>
2023-05-12 00:17:23 -0700

    modify

--- client/src/components/Ratings/sub-comps/ReviewsList.jsx
+++ client/src/components/Ratings/sub-comps/ReviewsList.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ReviewTile from './ReviewTile';
 
 function ReviewsList({ reviews }) {
   const [scrollTop, setScrollTop] = useState(0);
+
   const style = {
     border: '1px solid black',
     width: '600px',
