4436521db9d6f0ebd212e18b15ab14407ec7c8fb
Dillon Armstrong <dstrongarm93@gmail.com>
2023-05-15 17:01:47 -0600

    working on testing

--- spec/Overview-spec/Overview.spec.jsx
+++ spec/Overview-spec/Overview.spec.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import RTL from '@testing-library/react';
+// import 'jest-environment-jsdom';
 import '@testing-library/jest-dom';
 import App from '../../client/src/components/App';
 import Overview from '../../client/src/components/Overview';
 
 describe('Overview', () => {
-  const { getByRole, getByTestId } = render(<App />);
-  const app = getByRole('application');
-  const overview = getByTestId('overview');
-  const name = getByTestId('productName');
-  const category = getByTestId('category');
-
   test('It should be a function', () => {
     expect(typeof Overview).toBe('function');
   });
-  test('it should render to the App', () => {
+  test('it should render to the App', async () => {
+    RTL.render(<App />);
+
+    fireEvent.click(RTL.screen.getByText('Load'));
+
+    const app = await RTL.screen.getByRole('application');
+    const overview = await RTL.screen.getByTestId('overview');
+    // const name = screen.getByTestId('productName');
+    // const category = screen.getByTestId('category');
+
     expect(app).toContainElement(overview);
   });
-  test('the Name of the product and category should be visible', () => {
+  xtest('the Name of the product and category should be visible', async () => {
+    RTL.render(<App />);
+    const name = await RTL.screen.getByTestId('productName');
     expect(name).toBeVisible();
-    expect(category).toBeVisible();
+    // cleanup();
   });
 });
