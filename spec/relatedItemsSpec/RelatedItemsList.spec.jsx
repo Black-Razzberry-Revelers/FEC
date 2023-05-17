49fb4ef831ca52757e3e56d2119154729d859c93
Andrew Lund <andrew.s.lund@gmail.com>
2023-05-16 20:37:43 -0700

    some tests for RelatedItemsList and Related

--- spec/relatedItemsSpec/RelatedItemsList.spec.jsx
+++ spec/relatedItemsSpec/RelatedItemsList.spec.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { currentItem, relatedItems } from './exampleData';
+import { requests } from '../../client/src/components/requests';
 
 import RelatedItemsList from '../../client/src/components/Related/subcomponents/RelatedItemsList';
 
+jest.mock('../../client/src/components/requests');
 jest.mock(
   '../../client/src/components/Related/subcomponents/ItemCard',
-  () => function mockItemCard() {
+  () => function mockItemCard({ item }) {
     return (
-      <div value="mock-item-card">
+      <div role="mock-item-card">
         <img
-          src="https://static-00.iconduck.com/assets.00/image-icon-256x256-09od4zyo.png"
+          src={item.styles.results[0].photos[0].thumbnail_url}
           alt="mock"
         />
-        <div>Some Fake Product</div>
+        <div>{`Some Fake Product ${item.product.id}`}</div>
       </div>
     );
   },
 );
 
+beforeEach(async () => {
+  const setProduct = jest.fn();
+  requests.get.related.mockResolvedValue({ data: relatedItems });
+  render(<RelatedItemsList currentProduct={currentItem} setProduct={setProduct} />);
+  expect(await screen.findAllByText(/Some Fake Product/)).toHaveLength(4);
+});
+
+afterEach(() => jest.resetAllMocks());
+
 describe('Related Item List', () => {
   it('should contain ItemCards as children', () => {
-    const setProduct = jest.fn();
-    render(<RelatedItemsList currentProduct={currentItem} setProduct={setProduct} />);
+    screen.findAllByRole('mock-item-card')
+      .then((results) => expect(results).toHaveLength(4));
+  });
 
-    screen.findAllByText('Mock Item Card')
-      .then((results) => (results.forEach(
-        (result) => expect(result).toBe('Blah Blah Blah'),
-      )));
+  it('should reveal left carousel button when right carousel button is clicked', () => {
+    const button = screen.getByRole('button', { name: '>' });
+    // const itemCards = screen.getAllByRole('mock-item-card');
+    expect(screen.getAllByRole('button', { name: /<||>/ })).toHaveLength(1);
+    fireEvent.click(button);
+    expect(screen.getAllByRole('button', { name: /<||>/ })).toHaveLength(2);
   });
 });
