b9140b0d6db5d0859dbd7f7236b41fc2c53517c2
Dillon Armstrong <dstrongarm93@gmail.com>
2023-05-20 12:48:35 -0600

    added packages and config  for css minification (prod only). will probably need to deploy to test

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,9 +1,24 @@
 const { merge } = require('webpack-merge');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const MiniCSS = require('css-minimizer-webpack-plugin');
 const common = require('./webpack.common.config');
 
 module.exports = merge(
   common,
   {
     mode: 'production',
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /.s?css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [new CssMinimizerPlugin()],
+    },
+    plugins: [new MiniCssExtractPlugin()],
   },
 );
