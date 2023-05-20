98cc2bc9ccc0d60077d6d0696f05bbda98359ad2
Dillon Armstrong <dstrongarm93@gmail.com>
2023-05-20 16:00:02 -0600

    need to install css-loader

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,26 +2,30 @@ const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const common = require('./webpack.common.config');
+const path = require('path');
 
 module.exports = merge(
   common,
   {
+    entry: path.join(__dirname, '/client/dist/style.css'),
     mode: 'production',
     devtool: 'source-map',
     module: {
       rules: [
         {
           test: /.s?css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
       ],
     },
+    plugins: [new MiniCssExtractPlugin({
+      filename: '[name].css',
+    })],
     optimization: {
-      minimize: true,
       minimizer: [new CssMinimizerPlugin({
         test: /\.foo\.css$/i,
       })],
+      minimize: true,
     },
-    plugins: [new MiniCssExtractPlugin()],
   },
 );
